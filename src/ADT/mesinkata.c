48fd27f7a005e28a31ba206d6b31fa8a386320b6
timothysubekti <16521418@mahasiswa.itb.ac.id>
2022-11-03 13:48:36 +0700

    revisi 1 mesin kata

--- src/ADT/mesinkata.c
+++ src/ADT/mesinkata.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#include "mesinkata.h"
+#include "mesinWord.h"
 
 boolean EndWord;
-Word currentWord;
+Word CWord;
+Word CCommand;
 
 void IgnoreBlanks()
 {
@@ -16,12 +17,12 @@ void IgnoreBlanks()
     }
 }
 
-void STARTWORD(char *FileName)
+void STARTGAME(char *FileName)
 {
 /* I.S. : CC sembarang
    F.S. : EndWord = true, dan CC = MARK;
-          atau EndWord = false, currentWord adalah kata yang sudah diakuisisi,
-          CC karakter pertama sesudah karakter terakhir kata */
+          atau EndWord = false, CWord adalah Word yang sudah diakuisisi,
+          CC karakter pertama sesudah karakter terakhir Word */
     START(FileName);
     IgnoreBlanks();
     
@@ -40,11 +41,11 @@ void STARTWORD(char *FileName)
 
 void ADVWORD()
 {
-/* I.S. : CC adalah karakter pertama kata yang akan diakuisisi
-   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi,
-          CC adalah karakter pertama dari kata berikutnya, mungkin MARK
+/* I.S. : CC adalah karakter pertama Word yang akan diakuisisi
+   F.S. : CWord adalah Word terakhir yang sudah diakuisisi,
+          CC adalah karakter pertama dari Word berikutnya, mungkin MARK
           Jika CC = MARK, EndWord = true.
-   Proses : Akuisisi kata menggunakan procedure SalinWord */
+   Proses : Akuisisi Word menggunakan procedure CopyWord */
     IgnoreBlanks();
     if(IsEOP())
     {
@@ -60,40 +61,129 @@ void ADVWORD()
 
 void CopyWord()
 {
-/* Mengakuisisi kata, menyimpan dalam currentWord
-   I.S. : CC adalah karakter pertama dari kata
-   F.S. : currentWord berisi kata yang sudah diakuisisi;
+/* Mengakuisisi Word, menyimpan dalam CWord
+   I.S. : CC adalah karakter pertama dari Word
+   F.S. : CWord berisi Word yang sudah diakuisisi;
           CC = BLANK atau CC = MARK;
           CC adalah karakter sesudah karakter terakhir yang diakuisisi.
-          Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
+          Jika panjang Word melebihi NMax, maka sisa Word "dipotong" */
     int i = 0;
-    while ((!IsEOP()) && (!IsBlank()))
+    while ((!IsEOP()) && (!IsBlank()) && (!IsEOPC()))
     {
         if (i < NMax)
         {
-        currentWord.TabWord[i] = CC;
+        CWord.TabWord[i] = CC;
         i++;
         }
         ADV();
         
     }
-    currentWord.Length = i;
+    CWord.Length = i;
 }
 
-void PrintWord(Word Currentword)
+void PrintWord(Word CWord)
 {
 /* Mencetak current word
-   I.S : Currentword tidak kosong dan terdefinisi
-   F.S : isi dari Currentword dicetak ke layar */
-   for (int i = 0 ; i < Currentword.Length ; i++)
+   I.S : CWord tidak kosong dan terdefinisi
+   F.S : isi dari CWord dicetak ke layar */
+   for (int i = 0 ; i < CWord.Length ; i++)
    {
-    printf("%c",Currentword.TabWord[i]);
+    printf("%c",CWord.TabWord[i]);
    }
    printf("\n");
 }
 
 boolean IsBlank()
 {
-/* Mengirimkan true jika currentWord = BLANK */
+/* Mengirimkan true jika CWord = BLANK */
     return (CC == BLANK);
+}
+
+/* *** ADT untuk baca commands *** */
+
+void IgnoreDot()
+{
+/* Mengabaikan satu atau beberapa BLANK dan MARK
+   I.S. : CC sembarang 
+   F.S. : CC ≠ BLANK atau CC = ENTER */
+    while (IsBlank() && IsEOP()) 
+    {
+        ADVC();
+    }
+}
+
+void STARTCOMMAND()
+{
+/* I.S. : CC sembarang 
+   F.S. : EndWord = true, dan CC = ENTER; 
+          atau EndWord = false, CCommand adalah Word yang sudah diakuisisi,
+          CC karakter pertama sesudah karakter terakhir Word */
+    COMMAND();
+    IgnoreDot();
+    if (IsEOPC())
+    {
+        EndWord = true;
+    } else {
+        EndWord = false;
+        ADVCOMMAND();
+    }
+}
+
+void ADVCOMMAND()
+{
+/* I.S. : CC adalah karakter pertama Word yang akan diakuisisi 
+   F.S. : CCommand adalah Word terakhir yang sudah diakuisisi, 
+          CC adalah karakter pertama dari Word berikutnya, mungkin ENTER
+          Jika CC = ENTER, EndWord = true.		  
+   Proses : Akuisisi Word menggunakan procedure CopyCommand */
+    IgnoreDot();
+    if (IsEOPC() && !EndWord){
+        EndWord = true;
+    } else{
+        CopyCommand();
+        IgnoreDot();
+    }
+}
+
+void CopyCommand()
+{
+/* Mengakuisisi Word, menyimpan dalam CCommand
+   I.S. : CC adalah karakter pertama dari Word
+   F.S. : CCommand berisi Word yang sudah diakuisisi; 
+          CC = BLANK atau CC = ENTER; 
+          CC adalah karakter sesudah karakter terakhir yang diakuisisi.
+          Jika panjang Word melebihi NMax, maka sisa Word "dipotong" */
+    int i = 0;
+    while (!IsBlank() && !IsEOPC()) 
+    {
+        if (i < NMax)
+        {
+        CCommand.TabWord[i] = CC;
+        i++;
+        }
+        ADVC();
+    }
+    CCommand.Length = i;
+}
+
+/* *** Additional Function *** */
+
+boolean IsWordSama(Word InputCommand, Word Command)
+{
+    /* Mengirimkan true jika K1 = K2 : Length dan elemen tiap arraynya sama */
+
+    boolean sama = true;
+    if (InputCommand.Length != Command.Length) {
+        return !sama;
+    } else {
+        int i = 0;
+        while (sama && (i < Command.Length)) {
+            if (InputCommand.TabWord[i] != Command.TabWord[i]) {
+                sama = false;
+            }
+            i++;
+        }
+        return sama;
+    }
+
 }
--- src/ADT/mesinkata.h
+++ src/ADT/mesinkata.h
@@ -1,60 +1,104 @@
-/* File: mesinkata.h */
-/* Definisi Mesin Kata: Model Akuisisi Versi I */
+/* File: mesinWord.h */
+/* Definisi Mesin Word: Model Akuisisi Versi I */
 
-#ifndef __MESINKATA_H__
-#define __MESINKATA_H__
+#ifndef __MESINWord_H__
+#define __MESINWord_H__
 
-#include "D:/Semester 3/ALSTRUKDAT/TUBES 1/Tubes-1-Alstrukdat-G11/src/boolean.h"
+#include "../boolean.h"
 #include "mesinkarakter.h"
 
-#define NMax 50
-#define BLANK '.'
+#define NMax 100
+#define BLANK ' '
 
 typedef struct
 {
-   char TabWord[NMax]; /* container penyimpan kata, indeks yang dipakai [0..NMax-1] */
+   char TabWord[NMax]; /* container penyimpan Word, indeks yang dipakai [0..NMax-1] */
    int Length;
 } Word;
 
-/* State Mesin Kata */
+/* State Mesin Word */
 extern boolean EndWord;
-extern Word currentWord;
+extern Word CWord;
+extern Word CCommand;
 
 void IgnoreBlanks();
 /* Mengabaikan satu atau beberapa BLANK
    I.S. : CC sembarang
    F.S. : CC ≠ BLANK atau CC = MARK */
 
-void STARTWORD(char *FileName);
+void STARTGAME(char *FileName);
 /* I.S. : CC sembarang
    F.S. : EndWord = true, dan CC = MARK;
-          atau EndWord = false, currentWord adalah kata yang sudah diakuisisi,
-          CC karakter pertama sesudah karakter terakhir kata */
+          atau EndWord = false, CWord adalah Word yang sudah diakuisisi,
+          CC karakter pertama sesudah karakter terakhir Word */
 
 void ADVWORD();
-/* I.S. : CC adalah karakter pertama kata yang akan diakuisisi
-   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi,
-          CC adalah karakter pertama dari kata berikutnya, mungkin MARK
+/* I.S. : CC adalah karakter pertama Word yang akan diakuisisi
+   F.S. : CWord adalah Word terakhir yang sudah diakuisisi,
+          CC adalah karakter pertama dari Word berikutnya, mungkin MARK
           Jika CC = MARK, EndWord = true.
-   Proses : Akuisisi kata menggunakan procedure SalinWord */
+   Proses : Akuisisi Word menggunakan procedure SalinWord */
 
 void CopyWord();
-/* Mengakuisisi kata, menyimpan dalam currentWord
-   I.S. : CC adalah karakter pertama dari kata
-   F.S. : currentWord berisi kata yang sudah diakuisisi;
+/* Mengakuisisi Word, menyimpan dalam CWord
+   I.S. : CC adalah karakter pertama dari Word
+   F.S. : CWord berisi Word yang sudah diakuisisi;
           CC = BLANK atau CC = MARK;
           CC adalah karakter sesudah karakter terakhir yang diakuisisi.
-          Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
+          Jika panjang Word melebihi NMax, maka sisa Word "dipotong" */
 
-void PrintWord(Word Currentword);
+void PrintWord(Word CWord);
 /* Mencetak current word
-   I.S : Currentword tidak kosong dan terdefinisi
-   F.S : isi dari Currentword dicetak ke layar */
+   I.S : CWord tidak kosong dan terdefinisi
+   F.S : isi dari CWord dicetak ke layar */
 
 boolean IsBlank();
-/* Mengirimkan true jika currentWord = BLANK */
+/* Mengirimkan true jika CWord = BLANK */
 
-void ADV_BLANK();
-/* Mengakuisisi Newline ke current character*/
+
+/* *** ADT untuk membaca commands *** */
+
+void IgnoreDot();
+/* Mengabaikan satu atau beberapa BLANK dan MARK
+   I.S. : CC sembarang 
+   F.S. : CC ≠ BLANK atau CC = ENTER */
+
+void STARTCOMMAND();
+/* I.S. : CC sembarang 
+   F.S. : EndWord = true, dan CC = ENTER; 
+          atau EndWord = false, CCommand adalah Word yang sudah diakuisisi,
+          CC karakter pertama sesudah karakter terakhir Word */
+
+void ADVCOMMAND();
+/* I.S. : CC adalah karakter pertama Word yang akan diakuisisi 
+   F.S. : CComand adalah Word terakhir yang sudah diakuisisi, 
+          CC adalah karakter pertama dari Word berikutnya, mungkin ENTER
+          Jika CC = ENTER, EndWord = true.		  
+   Proses : Akuisisi Word menggunakan procedure SalinCommand */
+
+void CopyCommand();
+/* Mengakuisisi Word, menyimpan dalam CComand
+   I.S. : CC adalah karakter pertama dari Word
+   F.S. : CComand berisi Word yang sudah diakuisisi; 
+          CC = BLANK atau CC = ENTER; 
+          CC adalah karakter sesudah karakter terakhir yang diakuisisi.
+          Jika panjang Word melebihi NMax, maka sisa Word "dipotong" */
+
+
+/* *** FUNGSI TAMBAHAN *** */
+
+boolean IsWordSama(Word InputCommand, Word Command);
+/* Mengirimkan true jika K1 = K2 : Length dan elemen tiap arraynya sama */
+
+void PrintWord(Word K);
+/* Mencetak Word ke layar
+   I.S. : Word K terdefinisi
+   F.S. : Word K tercetak pada layar */
+
+int stringLength (char* string);
+/* Mengirimkan panjang sebuah string */
+
+Word toWord(char* command);
+/* Mengirimkan Word yang elemen of arraynya berasal dari command */
 
 #endif
