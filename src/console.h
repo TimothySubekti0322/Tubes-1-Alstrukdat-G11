33b9974bde36bda2041cfd26f394589487039107
Kenazizan01 <18221107@std.stei.itb.ac.id>
2022-11-04 17:09:28 +0700

    Update console.h masukin fungsi STARTBNMO

---
+++ src/console.h
@@ -0,0 +1,10 @@
+#ifndef __CONSOLE__
+#define __CONSOLE__
+
+#include "ADT/array/arraydinamik.h"
+#include "ADT/mesinkarakter/mesinkarakter.h"
+#include "ADT/mesinkata/mesinkata.h"
+
+void STARTBNMO(DynArray *gamesBNM0);
+
+#endif
