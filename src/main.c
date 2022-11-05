39ef8715082b5d1e365a2dcbaa82b6af85f5d7d8
Kenazizan01 <18221107@std.stei.itb.ac.id>
2022-11-05 09:38:00 +0700

    Update main.c

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ADT/mesinkarakter/mesinkarakter.h"
+#include "ADT/mesinkata/mesinkata.h"
+
+
+void welcome(){
+    char *screen =
+     "██████  ███    ██ ███    ███  ██████ \n"
+     "██   ██ ████   ██ ████  ████ ██    ██  \n"
+     "██████  ██ ██  ██ ██ ████ ██ ██    ██ \n"
+     "██   ██ ██  ██ ██ ██  ██  ██ ██    ██ \n"
+     "██████  ██   ████ ██      ██  ██████ ' \n";
+     
+
+     char *text =
+     "WELCOME TO BNMO <33\n"
+     "START :: MAININ BNMO DONG SAYANG\n"
+     "LOAD  :: LOAD GAME YANG TERSEDIA YUK SAYANG\n";
+
+
+     printf("%s",screen);
+     printf(" \n");
+     printf("============================================================================================================================\n");
+     printf(" \n");
+     printf("%s",text);
+}
+
+int main(){
+    welcome();
+    
+}
