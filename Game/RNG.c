f55c26ca4ec336075baf0e14d1c06763fe7c07a8
timothysubekti <16521418@mahasiswa.itb.ac.id>
2022-11-08 11:09:44 +0700

    Memasukan Game RNG ke Folder Game

---
+++ Game/RNG.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "RNG.h"
+void gameRNG()  
+{  
+    srand(time(NULL));
+    printf("RNG Telah dimulai. Uji keberuntungan Anda dengan menebak sebuah angka X yang rentangnya berada diantara 1 - 10.\n");
+    int MaxTry = 10;
+    int tebakan;
+    int skor = 0;
+    int Target = rand() % 10 + 1;
+    printf("%d\n",Target); //Cheat Code - Buat Tahu Jawabannya :)
+    printf("Tebakan : ");
+    INPUT();
+    WordToInt(CWord,&tebakan);
+    int nTebakan = 1;
+    while((tebakan != Target) && (nTebakan <= MaxTry)){
+        if(tebakan < Target){
+            printf("Lebih Besar\n");
+        }
+        else if(tebakan > Target){
+            printf("Lebih Kecil\n");
+        }
+        tebakan = 0;
+        printf("Tebakan : ");
+        INPUT();
+        WordToInt(CWord,&tebakan);
+        nTebakan++;
+    }
+    if(nTebakan == MaxTry){
+        printf("GAME OVER!\n");
+    }
+    else{
+        printf("YA , X Adalah %d\n" , Target);
+    }
+    skor = MaxTry - nTebakan + 1;
+    printf("skor = %d" , skor);
+    //return skor;
+}
+
+/*TEST CASE*/
+
+// int main(){
+//     gameRNG();
+//     return 0;
+// }
---
+++ Game/RNG.h
@@ -0,0 +1,18 @@
+/* File: RNG.h */
+/* Definisi Games RNG */
+
+#ifndef RNG_H
+#define RNG_H
+
+#include "mesinkarakter.h"
+#include "mesinkata.h"
+
+void gameRNG();
+/* Game RNG
+   Game menebak suatu Angka Acak X yang berada para rentang 1 - 10. . Di setiap giliran, user diberi 
+   kesempatan menebak angka X dan program akan memberi  tahu apakah tebakan pemain dibandingkan 
+   terhadap X lebih besar atau lebih kecil User memiliki kesempatan untuk menebak sebanyak 10 kali
+   Permainan berakhir jika pemain menebak angka X dengan benar. Skor game akan muncul di akhir Game 
+   berdasarkan seberapa cepat pemain menebak X.*/
+
+#endif
