40a6ccf53ef648178643be9ef8ca4fb1b031f07b
Nicholas <nicholas13245768@gmail.com>
2022-11-09 23:18:44 +0700

    mole bonus

---
+++ Game/mole.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include "time.h"
+#include <stdlib.h>
+#include "Tubes-1-Alstrukdat-G11/src/ADT/array/arraydinamikstr.c"
+#include "Tubes-1-Alstrukdat-G11/src/boolean.h"
+#include "Tubes-1-Alstrukdat-G11/src/console.c"
+#include "Tubes-1-Alstrukdat-G11/src/ADT/queue/queue.c"
+#include "Tubes-1-Alstrukdat-G11/src/ADT/mesinkata/mesinkata.c"
+#include "Tubes-1-Alstrukdat-G11/src/ADT/mesinkarakter/mesinkarakter.c"
+#include "Tubes-1-Alstrukdat-G11/src/ADT/queue/queuestr.c" // Somehow diminta??????
+#include "Tubes-1-Alstrukdat-G11/Game/dinerdash.c"
+#include "Tubes-1-Alstrukdat-G11/Game/RNG.c"
+
+typedef struct{
+    ArrayDyn a;
+    ArrayDyn b;
+    ArrayDyn c;
+    ArrayDyn d;
+    ArrayDyn e;
+} Board;
+
+Board CreateBoard(){
+    Board papan;
+    papan.a = CreateStrArrayDyn();
+    papan.b = CreateStrArrayDyn();
+    papan.c = CreateStrArrayDyn();
+    papan.d = CreateStrArrayDyn();
+    papan.e = CreateStrArrayDyn();
+    int i;
+    for(i = 0;i<5;i++){
+        InsertStrLast(&papan.a,"O");
+        InsertStrLast(&papan.b,"O");
+        InsertStrLast(&papan.c,"O");
+        InsertStrLast(&papan.d,"O");
+        InsertStrLast(&papan.e,"O");
+    }
+
+    return papan;
+
+}
+
+void randomnumber(int *x,int *y){
+    srand(time(NULL));
+    *x = rand() % 5 + 1;
+    *y = rand() % 5 + 1;
+}
+
+void UbahBoard(Board *papan,int x,int y){
+    if(y == 1){
+        DeleteStrIn(&papan->a,x-1);
+        InsertStrIn(&papan->a,"X",x-1);
+    } else if (y == 2){
+        DeleteStrIn(&papan->b,x-1);
+        InsertStrIn(&papan->b,"X",x-1);
+    } else if (y == 3){
+        DeleteStrIn(&papan->c,x-1);
+        InsertStrIn(&papan->c,"X",x-1);
+    } else if (y == 4){
+        DeleteStrIn(&papan->d,x-1);
+        InsertStrIn(&papan->d,"X",x-1);
+    } else if (y == 5){
+        DeleteStrIn(&papan->e,x-1);
+        InsertStrIn(&papan->e,"X",x-1);
+    }
+}
+
+boolean IsValid(char *coordinates){
+    return ((coordinates[0]>='1' && coordinates[0] <= '5') && (coordinates[2]>='1' && coordinates[2] <= '5') && stringLength(coordinates)==3);
+}
+
+boolean IsBlankGrid(Board papan,int x,int y){
+    if(y == 1){
+        char *n = GetStr(papan.a, x-1); 
+        return n == "O";
+    } else if(y ==2){
+        char *n = GetStr(papan.b, x-1); 
+        return n == "O";
+    } else if(y == 3){
+        char *n = GetStr(papan.c, x-1); 
+        return n == "O";
+    } else if(y ==4){
+        char *n = GetStr(papan.d, x-1); 
+        return n == "O";
+    } else if(y == 5){
+        char *n = GetStr(papan.e, x-1); 
+        return n == "O";
+    }
+    return false;
+}
+int xcoor(char *coordinates){
+    return coordinates[0]-48;
+}
+
+int ycoor(char *coordinates){
+    return coordinates[2]-48;
+}
+
+void PrintPapan(Board papan){
+    for(int i = 0;i<5;i++){
+        printf("%s ",papan.a.Ar[i]);
+    }
+    printf("\n");
+    for(int i = 0;i<5;i++){
+        printf("%s ",papan.b.Ar[i]);
+    }
+    printf("\n");
+    for(int i = 0;i<5;i++){
+        printf("%s ",papan.c.Ar[i]);
+    }
+    printf("\n");
+    for(int i = 0;i<5;i++){
+        printf("%s ",papan.d.Ar[i]);
+    }
+    printf("\n");
+    for(int i = 0;i<5;i++){
+        printf("%s ",papan.e.Ar[i]);
+    }
+    printf("\n");
+}
+
+
+void mole(){
+    int score = 25;
+    boolean gameover = false;
+    Board papan;
+    int x,y,xmole,ymole;
+    char *coordinates = "6 6";
+    papan = CreateBoard();
+    while(!gameover){
+        PrintPapan(papan);
+        randomnumber(&xmole,&ymole);
+        while(!IsBlankGrid(papan,xmole,ymole)){
+            randomnumber(&xmole,&ymole);
+        }
+        printf("%d %d cheat hehe\n",xmole,ymole);
+        while(!IsValid(coordinates)){
+            printf("Masukkan input dengan format 'x y', contoh 2 5: \n");
+            INPUT();
+            coordinates = wordToString(CWord);
+            x = xcoor(coordinates);
+            y = ycoor(coordinates);
+            GetStr(papan.e, x-1);
+            if(!IsBlankGrid(papan,x,y)){
+                coordinates = "6 6";
+            }
+        }
+        
+        if(x == xmole && y == ymole){
+            printf("Selamat Anda sudah menemukan molenya. Skor Anda %d\n",score);
+            gameover = true;
+        } else{
+            UbahBoard(&papan,x,y);
+            score -= 1;
+        }
+        coordinates = "6 6";
+    }
+}
+
+int main(){
+    mole();
+}
