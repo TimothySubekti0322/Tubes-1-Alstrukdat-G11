bfc81d860814715e17beaf5c0b8681c641fd5973
timothysubekti <16521418@mahasiswa.itb.ac.id>
2022-11-11 01:01:52 +0700

    Hapus arraydinamik

--- bin/BNMO.sh
+++ bin/BNMO.sh
@@ -1,5 +1,5 @@
 #BNMO.sh
 main :
-    gcc ../src/main.c ../src/console.c ../Game/dinerdash.c ../Game/RNG.c ../src/ADT/array/arraydinamikstr.c ../src/ADT/array/arraydinamik.c ../src/ADT/mesinkarakter/mesinkarakter.c ../src/ADT/mesinkata/mesinkata.c ../src/ADT/queue/queue.c ../src/ADT/queue/queuestr.c ../Game/mole.c ../src/ADT/array/arraystatis.c -o main
+    gcc ../src/main.c ../src/console.c ../Game/dinerdash.c ../Game/RNG.c ../src/ADT/mesinkarakter/mesinkarakter.c ../src/ADT/mesinkata/mesinkata.c ../src/ADT/queue/queue.c ../src/ADT/queue/queuestr.c ../Game/mole.c ../src/ADT/array/arraystatis.c -o main
 clean :
     rm main
--- src/console.h
+++ src/console.h
@@ -1,7 +1,7 @@
 #ifndef __CONSOLE__
 #define __CONSOLE__
 
-#include "ADT/array/arraydinamik.h"
+// #include "ADT/array/arraydinamik.h"
 #include "ADT/array/arraydinamikstr.h"
 #include "ADT/mesinkarakter/mesinkarakter.h"
 #include "ADT/mesinkata/mesinkata.h"
