cb4c3919b8ebfe13c22dacc5ddeae372bd80a88f
Nicholas <nicholas13245768@gmail.com>
2022-11-10 10:25:22 +0700

    Update dinerdash.h

--- Game/dinerdash.h
+++ Game/dinerdash.h
@@ -18,6 +18,7 @@ typedef struct{
         int durasi[10];
         int count;
 } masaksaji;
+// Berfungsi sebagai penyimpan makanan dalam proses pemasakan dan penyajian
 
 typedef struct{
     Queue indeks;
@@ -25,38 +26,61 @@ typedef struct{
     int ketahanan[25];
     int harga[25];
 } customers;
+// Berfungsi sebagai penyimpan customer dalam queue dan info infonya
 
 
 void IsMemberMasakSaji(masaksaji m,int custnumber,boolean *found,int *indeks);
 /*Mengecek jika sutu custnumber merupakan member dari masak atau saji dan 
-meletakkan status dari hasil pencarian di found dan indeks ditemukannya di indeks*/
+meletakkan status dari hasil pencarian di found dan indeks ditemukannya di indeks
+I.S. masaksaji m terdefinisi, custnumber sudah diinput dan found dan indeks terdefinisi
+sebagai penampung hasil
+F.S. found jika ditemukan custnumber pada masak atau saji, indeks ditemukannya 
+custnumber dikembalikan pada parameter indeks */
 
 void masaksajiempty(masaksaji *m);
-   /*Inisialisasi struct masak atau saji yang kosong*/
+/*Inisialisasi struct masak atau saji yang kosong
+I.S. m terdefinisi
+F.S. dibuat masaksaji m yang kosong*/
 
 
 int strlength(char *sentence);
-/*Mencari panjang dari sebuah string, merupakan pengganti dari strlen*/
+/*Mencari panjang dari sebuah string, merupakan pengganti dari strlen
+ I.S. sentence terdefinisi, sentence bisa saja kosong
+ F.S. direturn panjang dari sentence*/
 
 boolean serves(char *command);
-/*Mengecek apabila command berisi petunjuk untuk serve*/
+/*Mengecek apabila command berisi petunjuk untuk serve
+I.S. command terdefinisi, bisa saja kosong
+F.S. dikembalikan status jika command berisi perintah untuk melakukan serve*/
 
 boolean cooks(char *command);
-/*Mengecek apabila command berisi petunjuk untuk cook*/
+/*Mengecek apabila command berisi petunjuk untuk cook
+I.S. command terdefinisi, bisa saja kosong
+F.S. dikembalikan status jika command berisi perintah untuk melakukan cook*/
 
 boolean skip(char *command);
-/*Mengecek apabila command berisi petunjuk untuk skip*/
+/*Mengecek apabila command berisi petunjuk untuk skip
+I.S. command terdefinisi, bisa saja kosong
+F.S. dikembalikan status jika command berisi perintah untuk melakukan skip*/
 
 int custnumbers(char *command);
-/*Mencari indeks customer yang akan di cook atau serve dari command*/
+/*Mencari indeks customer yang akan di cook atau serve dari command
+I.S. command terdefinisi, bisa saja kosong
+F.S. Dikembalikan indeks dari customer dari command*/
 
 void DeleteMasakSaji(masaksaji *m,int indeks);
-/*Melakukan penghapusan suatu indeks pada struct masak atau saji*/
+/*Melakukan penghapusan suatu indeks pada struct masak atau saji
+I.S. m terdefinisi, m tidak kosong, indeks sudah diinput
+F.S. Dihapus info makanan yang dimasak pada indeks di masaksaji*/
 
 void tambahturn(masaksaji *masak, masaksaji *saji,customers *orders);
 /*Melakukan penambahan turn dengan menambah satu customer, mengurangi durasi memasak
 dan mengurangi ketahanan saji, lalu memindahkan makanan pada struct masak dengan durasi 0 
-dan menghilangkan makanan pada struct saji dengan ketahanan 0*/
+dan menghilangkan makanan pada struct saji dengan ketahanan 0
+I.S. Game sudah dimulai, masak, saji, dan orders terdefinisi.
+F.S. Dilakukan operasi penambahan turn, durasi memasak dikurangi 1, customer ditambah,
+ketahanan saji dikurangi satu, dan memindahkan makanan dari masak ke saji jika durasi sudah 0 dan 
+makanan dihilangkan jika ketahanan sudah 0*/
 
 void dinnerdash();
 /*Memulai game dinerdash yang awalnya menampilkan isi antrian,makanan yang dimasak, makanan yang dapat disaji
