c33015bc7964d19743842c864632eb06ca2a2f36
timothysubekti <16521418@mahasiswa.itb.ac.id>
2022-11-11 02:12:19 +0700

    Update Readme dan menambahkan rm command pada BNMO.sh

--- bin/BNMO.sh
+++ bin/BNMO.sh
@@ -1,2 +1,2 @@
 #BNMO.sh
-gcc ../src/main.c ../src/console.c ../Game/dinerdash.c ../Game/RNG.c ../src/ADT/array/arraydinamikstr.c ../src/ADT/mesinkarakter/mesinkarakter.c ../src/ADT/mesinkata/mesinkata.c ../src/ADT/queue/queue.c ../src/ADT/queue/queuestr.c ../Game/mole.c ../src/ADT/array/arraystatis.c -o main && ./main
+gcc ../src/main.c ../src/console.c ../Game/dinerdash.c ../Game/RNG.c ../src/ADT/array/arraydinamikstr.c ../src/ADT/mesinkarakter/mesinkarakter.c ../src/ADT/mesinkata/mesinkata.c ../src/ADT/queue/queue.c ../src/ADT/queue/queuestr.c ../Game/mole.c ../src/ADT/array/arraystatis.c -o main && ./main && rm main.exe
