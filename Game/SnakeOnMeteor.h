37b9118fec7ff45ca7b34753b46445a746796303
Shulhann <18221051@std.stei.itb.ac.id>
2022-11-27 01:20:49 +0700

    Update SnakeOnMeteor.h

--- Game/SnakeOnMeteor.h
+++ Game/SnakeOnMeteor.h
@@ -15,12 +15,20 @@ void randomnumber(int *x,int *y);
 
 void GenerateFood(StatArray *container, int *FoodLocation);
 
+void GenerateMeteor(int *MeteorLocation, int Foodlocation);
+
 boolean IsPointNotEmpty(List snake, int input);
 
-void ShowSquare(List snake, int *FoodLocation, boolean *IsFoodAvailable);
+boolean IsSnakeBody(List snake, int input);
+
+int ShowSquare(StatArray container);
+
+void NextTurn(List *snake, int *FoodLocation, boolean *IsFoodAvailable, int Turn, int *MeteorLocation, int *End, int *score)
 
 void CreateSnake(List *snake);
 
 boolean IsInputValid(Word CWord);
 
 boolean IsMoveable(Word CWord, List snake);
+
+boolean IsMeteorLocation(Word Cword, List snake, int MeteorLocation);
